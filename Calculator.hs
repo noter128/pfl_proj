f1bab252e376bba926809b265dfe748306eec848
noter128 <163766616+noter128@users.noreply.github.com>
2025-11-13 23:17:42 +0000

    part 2 done

--- Calculator.hs
+++ Calculator.hs
@@ -15,22 +15,33 @@ import Data.Char
 -- made up from integer numbers, + and *
 --
 data Expr = Num Integer
+          | Var Name -- Part 2 variable reference
           | Add Expr Expr
           | Sub Expr Expr
           | Mul Expr Expr
           | Div Expr Expr
           | Mod Expr Expr
           deriving Show
+-- Part 2 type and data create
+type Name = String
+type Env = [(Name, Integer)]
+
+data Command = Assign Name Expr -- x = expr
+              | EvalCmd Expr -- just an expression command
+              deriving Show
 
 -- a recursive evaluator for expressions
 --
-eval :: Expr -> Integer
-eval (Num n) = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Sub e1 e2) = eval e1 - eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
-eval (Div e1 e2) = eval e1 `div` eval e2
-eval (Mod e1 e2) = eval e1 `mod` eval e2
+eval :: Env -> Expr -> Integer
+eval env (Num n) = n
+eval env (Var x) = case lookup x env of -- part2
+                  Just v -> v -- part2
+                  Nothing -> error ("undefined variable: " ++ x) -- part2
+eval env (Add e1 e2) = eval env e1 + eval env e2 -- add env to everything 
+eval env (Sub e1 e2) = eval env e1 - eval env e2
+eval env (Mul e1 e2) = eval env e1 * eval env e2
+eval env (Div e1 e2) = eval env e1 `div` eval env e2
+eval env (Mod e1 e2) = eval env e1 `mod` eval env e2
 
 -- | a parser for expressions
 -- Grammar rules:
@@ -78,8 +89,13 @@ termCont acc =  do
                  <|> return acc
 
 factor :: Parser Expr
-factor = do n <- natural
-            return (Num n)
+factor =  (do 
+            x <- variable
+            return (Var x)) --Part 2 add variable to factor
+          <|>
+          (do 
+            n <- natural
+            return (Num n))
           <|>
           do char '('
              e <- expr
@@ -91,22 +107,60 @@ natural :: Parser Integer
 natural = do xs <- many1 (satisfy isDigit)
              return (read xs)
 
+variable :: Parser Name --Part 2 create variable function
+variable = do
+             xs <- many1 (satisfy isLetter)
+             return xs
+
+command :: Parser Command --Part 2 create command function
+command = (do
+             x <- variable
+             char '='
+             e <- expr
+             return (Assign x e))
+          <|> (do
+                 e <- expr
+                 return (EvalCmd e))
+
+update :: Name -> Integer -> Env -> Env --Part 2 create update function
+update x v env = (x, v) : filter (\(y,_) -> y /= x) env
+
+runCommand :: Env -> Command -> (Integer, Env) --Part 2 create runCommand func
+runCommand env (EvalCmd e) =
+  let v = eval env e
+  in (v, env)
+runCommand env (Assign x e) =
+  let v = eval env e
+      env' = update x v env
+  in (v, env')
 ----------------------------------------------------------------             
   
 main :: IO ()
 main
-  = do txt <- getContents
-       calculator (lines txt)
+  = do 
+      txt <- getContents
+      calculator [] (lines txt) --Part 2 add []
 
 -- | read-eval-print loop
-calculator :: [String] -> IO ()
-calculator []  = return ()
-calculator (l:ls) = do putStrLn (evaluate l)
-                       calculator ls  
+calculator :: Env -> [String] -> IO () --Part 2 add Env -> before [String]
+calculator _ []  = return () --Part 2 add _ before []
+calculator env (l:ls) = do --Part 2 add env and redo func
+                          let (out, env') = process env l
+                          putStrLn out
+                          calculator env' ls  
 
 -- | evaluate a single expression
-evaluate :: String -> String
+{-evaluate :: String -> String --Part 2 replace this evaluate func for process below
 evaluate txt
   = case parse expr txt of
       [ (tree, "") ] ->  show (eval tree)
-      _ -> "parse error; try again"  
+      _ -> "parse error; try again"-}  
+
+process :: Env -> String -> (String, Env) --Part 2 create process func -
+process env txt =
+  case parse command txt of
+    [(cmd, "")] ->
+      let (v, env') = runCommand env cmd
+      in  (show v, env')
+    _ ->
+      ("parse error; try again", env)
